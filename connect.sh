46725675774d778e48db764e493f97df45ddf8aa
John Hoffman <jah5@princeton.edu>
2018-07-23 18:35:51 -0400

    simplified things

--- connect.sh
+++ connect.sh
@@ -28,6 +28,39 @@ usage() {
     exit 1
 }
 
+
+is_connected() {
+    port=${1}
+    for pid in `pgrep -f "ssh -fNT -L $port"`; do
+        if [[ $pid != "" ]]; then
+            echo "connected"
+        fi
+    done
+}
+
+disconnect_port(){
+    port=${1}
+    for pid in `pgrep -f "ssh -fNT -L $port"`; do
+        if [[ $pid == "" ]]; then
+            continue
+        fi
+
+        echo "[port $port] killing pid $pid"
+        kill -9 $pid
+    done
+}
+
+connect_port(){
+    port=${1}
+    host=${2}
+    if [[ $(is_connected $port) == "connected" ]]; then
+        echo "Port $port is already connected."
+    else
+        echo "== forwarding $port@$host -> localhost:$port  =="
+        ssh -fNT -L ${1}:localhost:${1} $2
+    fi
+}
+
 # process (port, host)
 process() {
     port=$1
